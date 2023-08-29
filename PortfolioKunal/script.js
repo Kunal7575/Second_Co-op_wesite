7ded084425089f8217d63ab455b1933fa2a3366c
Kunal7575 <93230333+Kunal7575@users.noreply.github.com>
2023-08-29 11:56:36 -0400

    Update script.js

--- PortfolioKunal/script.js
+++ PortfolioKunal/script.js
@@ -1,40 +1,40 @@
-let words=document.querySelectorAll(".word");
-words.forEach((word)=>{
-    let letters=word.textContent.split("");
-    word.textContent="";
-    letters.forEach((letter)=>{
-        let span=document.createElement("span");
-        span.textContent=letter;
-        span.className="letter";
-        word.append(span);
-    });
-});
+// let words=document.querySelectorAll(".word");
+// words.forEach((word)=>{
+//     let letters=word.textContent.split("");
+//     word.textContent="";
+//     letters.forEach((letter)=>{
+//         let span=document.createElement("span");
+//         span.textContent=letter;
+//         span.className="letter";
+//         word.append(span);
+//     });
+// });
 
 
-let currentWordIndex=0;
-let maxWordIndex=words.length -1;
-words[currentWordIndex].style.opacity="1";
+// let currentWordIndex=0;
+// let maxWordIndex=words.length -1;
+// words[currentWordIndex].style.opacity="1";
 
-let changeText=()=>{
-    let currentWord=words[currentWordIndex];
-    let nextWord=currentWordIndex===maxWordIndex ? words[0]:words[currentWordIndex+1];
+// let changeText=()=>{
+//     let currentWord=words[currentWordIndex];
+//     let nextWord=currentWordIndex===maxWordIndex ? words[0]:words[currentWordIndex+1];
 
-    Array.from(currentWord.children).forEach((letter,i)=>{
-        setTimeout(()=>{
-            letter.className="letter out";
-        },i * 80);
-    });
-    nextWord.style.opacity="1";
-    Array.from(nextWord.children).forEach((letter,i)=>{
-        letter.className="letter behind";
-        setTimeout(()=>{
-            letter.className="letter in";
-        },340 + i * 80);
-    });
-    currentWordIndex=currentWordIndex===maxWordIndex ? 0 : currentWordIndex+1;
-};
-changeText();
-setInterval(changeText,3000)
+//     Array.from(currentWord.children).forEach((letter,i)=>{
+//         setTimeout(()=>{
+//             letter.className="letter out";
+//         },i * 80);
+//     });
+//     nextWord.style.opacity="1";
+//     Array.from(nextWord.children).forEach((letter,i)=>{
+//         letter.className="letter behind";
+//         setTimeout(()=>{
+//             letter.className="letter in";
+//         },340 + i * 80);
+//     });
+//     currentWordIndex=currentWordIndex===maxWordIndex ? 0 : currentWordIndex+1;
+// };
+// changeText();
+// setInterval(changeText,3000)
 
 
 
@@ -83,41 +83,41 @@ setInterval(changeText,3000)
 // });
 
 // active menu
-let menuLi=document.querySelectorAll('header ul li a');
-let section=document.querySelectorAll('section');
+// let menuLi=document.querySelectorAll('header ul li a');
+// let section=document.querySelectorAll('section');
 
-function activeMenu(){
-    let len=section.length;
-    while(--len && window.scrollY + 97 < section[len].offsetTop){}
-    menuLi.forEach(sec=>sec.classList.remove("active"));
-    menuLi[len].classList.add("active");
-}
+// function activeMenu(){
+//     let len=section.length;
+//     while(--len && window.scrollY + 97 < section[len].offsetTop){}
+//     menuLi.forEach(sec=>sec.classList.remove("active"));
+//     menuLi[len].classList.add("active");
+// }
 
-activeMenu();
-window.addEventListener("scroll",activeMenu);
-
-
+// activeMenu();
+// window.addEventListener("scroll",activeMenu);
 
 
 
-// navbar stick karne kay liye
-const header=document.querySelector("header");
-window.addEventListener("scroll",function(){
-    header.classList.toggle("sticky",this.window.scrollY>50)
-})
 
-let menuIcon=document.querySelector("#menu-icon");
-let navlist=document.querySelector("#.navlist");
 
-menuIcon.onClick=()=>{
-    menuIcon.classList.toggle("bx-x");
-    navlist.classList.toggle("open");
+// // navbar stick karne kay liye
+// const header=document.querySelector("header");
+// window.addEventListener("scroll",function(){
+//     header.classList.toggle("sticky",this.window.scrollY>50)
+// })
 
-}
-window.onscroll=()=>{
-    menuIcon.classList.remove("bx-x");
-    navlist.classList.remove("open");
-}
+// let menuIcon=document.querySelector("#menu-icon");
+// let navlist=document.querySelector("#.navlist");
+
+// menuIcon.onClick=()=>{
+//     menuIcon.classList.toggle("bx-x");
+//     navlist.classList.toggle("open");
+
+// }
+// window.onscroll=()=>{
+//     menuIcon.classList.remove("bx-x");
+//     navlist.classList.remove("open");
+// }
 
 
 
@@ -132,4 +132,3 @@ window.onscroll=()=>{
 
 
 // clock
-
